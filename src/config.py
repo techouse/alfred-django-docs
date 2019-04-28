8c2157fb5b378398b4c129f4eff42c5780c06c93
Klemen Tušar <techouse@gmail.com>
2019-04-28 16:16:16 +0200

    Cosmetics Reformat the code using [Black](https://github.com/ambv/black)

--- src/config.py
+++ src/config.py
@@ -7,11 +7,11 @@ class Config(object):
     # How long to cache results for
     CACHE_MAX_AGE = 20  # seconds
     # Icon
-    DJANGO_ICON = 'icon.png'
+    DJANGO_ICON = "icon.png"
     # supported docs
-    SUPPORTED_DJANGO_VERSIONS = {'v2.1', 'v1.11'}
-    DEFAULT_DJANGO_VERSION = '2.1'
+    SUPPORTED_DJANGO_VERSIONS = {"v2.1", "v1.11"}
+    DEFAULT_DJANGO_VERSION = "2.1"
     # Algolia credentials
-    ALGOLIA_APP_ID = 'WODHKE4WZG'
-    ALGOLIA_SEARCH_ONLY_API_KEY = '7456cdd91ba8d4f87846549697397759'
-    ALGOLIA_SEARCH_INDEX = 'docs'
+    ALGOLIA_APP_ID = "WODHKE4WZG"
+    ALGOLIA_SEARCH_ONLY_API_KEY = "7456cdd91ba8d4f87846549697397759"
+    ALGOLIA_SEARCH_INDEX = "docs"
--- src/docs.py
+++ src/docs.py
@@ -16,8 +16,7 @@ from workflow import Workflow, ICON_WARNING
 h = HTMLParser()
 
 # Algolia client
-client = algoliasearch.Client(Config.ALGOLIA_APP_ID,
-                              Config.ALGOLIA_SEARCH_ONLY_API_KEY)
+client = algoliasearch.Client(Config.ALGOLIA_APP_ID, Config.ALGOLIA_SEARCH_ONLY_API_KEY)
 index = client.init_index(Config.ALGOLIA_SEARCH_INDEX)
 
 # log
@@ -26,11 +25,11 @@ log = None
 
 def cache_key(query, version=Config.DEFAULT_DJANGO_VERSION):
     """Make filesystem-friendly cache key"""
-    key = query + '_' + version
+    key = query + "_" + version
     key = key.lower()
-    key = re.sub(r'[^a-z0-9-_;.]', '-', key)
-    key = re.sub(r'-+', '-', key)
-    log.debug('Cache key : {!r} {!r} -> {!r}'.format(query, version, key))
+    key = re.sub(r"[^a-z0-9-_;.]", "-", key)
+    key = re.sub(r"-+", "-", key)
+    log.debug("Cache key : {!r} {!r} -> {!r}".format(query, version, key))
     return key
 
 
@@ -38,19 +37,21 @@ def handle_result(api_dict):
     """Extract relevant info from API result"""
     result = {}
 
-    for key in {'version', 'id', 'title', 'permalink' 'categories', 'content'}:
+    for key in {"version", "id", "title", "permalink" "categories", "content"}:
         result[key] = h.unescape(api_dict[key])
 
     return result
 
 
-def search(query=None, version=Config.DEFAULT_DJANGO_VERSION, limit=Config.RESULT_COUNT):
+def search(
+    query=None, version=Config.DEFAULT_DJANGO_VERSION, limit=Config.RESULT_COUNT
+):
     if query:
-        results = index.search(query, {'filters': 'version=' + version,
-                                       'page': 0,
-                                       'hitsPerPage': limit})
-        if results is not None and 'hits' in results:
-            return results['hits']
+        results = index.search(
+            query, {"filters": "version=" + version, "page": 0, "hitsPerPage": limit}
+        )
+        if results is not None and "hits" in results:
+            return results["hits"]
     return []
 
 
@@ -58,77 +59,78 @@ def main(wf):
     query = wf.args[0].strip()
 
     # Tag prefix only. Treat as blank query
-    if query == 'v':
-        query = ''
+    if query == "v":
+        query = ""
 
-    log.debug('query : {!r}'.format(query))
+    log.debug("query : {!r}".format(query))
 
     if not query:
-        wf.add_item('Search the Django documentation')
+        wf.add_item("Search the Django documentation")
         wf.send_feedback()
         return 0
 
     # Parse query into query string and tags
-    words = query.split(' ')
+    words = query.split(" ")
 
     query = []
     version = Config.DEFAULT_DJANGO_VERSION
 
     for word in words:
         if word in Config.SUPPORTED_DJANGO_VERSIONS:
-            version = word.replace('v', '')
+            version = word.replace("v", "")
         else:
             query.append(word)
 
-    query = ' '.join(query)
+    query = " ".join(query)
 
     key = cache_key(query, version)
 
-    results = wf.cached_data(key, functools.partial(search, query, version),
-                             max_age=Config.CACHE_MAX_AGE)
+    results = wf.cached_data(
+        key, functools.partial(search, query, version), max_age=Config.CACHE_MAX_AGE
+    )
 
-    log.debug('{} results for {!r}, version {!r}'.format(len(results),
-                                                         query,
-                                                         version))
+    log.debug("{} results for {!r}, version {!r}".format(len(results), query, version))
     # Show results
     if not results:
-        wf.add_item('No matching answers found',
-                    'Try a different query',
-                    icon=ICON_WARNING)
+        wf.add_item(
+            "No matching answers found", "Try a different query", icon=ICON_WARNING
+        )
 
     for result in results:
-        if result['id'].startswith('django.'):
-            title = result['id']
-        elif result['id'].startswith('setting-'):
-            title = '{} [setting]'.format(result['title'])
-        elif result['id'].startswith('templatefilter-'):
-            title = '{} [template filter]'.format(result['title'])
-        elif result['id'].startswith('fieldlookup-'):
-            title = '{} [field lookup]'.format(result['title'])
-        elif result['id'].startswith('templatetag-'):
-            title = '{} [template tag]'.format(result['title'])
-        elif result['id'].startswith('cmdoption-'):
-            title = '{} [cmd option]'.format(result['title'])
-        elif result['id'].startswith('envvar-'):
-            title = '{} [env var]'.format(result['title'])
+        if result["id"].startswith("django."):
+            title = result["id"]
+        elif result["id"].startswith("setting-"):
+            title = "{} [setting]".format(result["title"])
+        elif result["id"].startswith("templatefilter-"):
+            title = "{} [template filter]".format(result["title"])
+        elif result["id"].startswith("fieldlookup-"):
+            title = "{} [field lookup]".format(result["title"])
+        elif result["id"].startswith("templatetag-"):
+            title = "{} [template tag]".format(result["title"])
+        elif result["id"].startswith("cmdoption-"):
+            title = "{} [cmd option]".format(result["title"])
+        elif result["id"].startswith("envvar-"):
+            title = "{} [env var]".format(result["title"])
         else:
-            title = result['title']
+            title = result["title"]
 
-        wf.add_item(uid=result['id'],
-                    title=title,
-                    subtitle=result['content'] if len(result['content']) else result['id'],
-                    arg=result['permalink'],
-                    valid=True,
-                    largetext=result['id'],
-                    copytext=result['id'],
-                    quicklookurl=result['permalink'],
-                    icon=Config.DJANGO_ICON)
+        wf.add_item(
+            uid=result["id"],
+            title=title,
+            subtitle=result["content"] if len(result["content"]) else result["id"],
+            arg=result["permalink"],
+            valid=True,
+            largetext=result["id"],
+            copytext=result["id"],
+            quicklookurl=result["permalink"],
+            icon=Config.DJANGO_ICON,
+        )
         # log.debug(result)
 
     wf.send_feedback()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     wf = Workflow()
     log = wf.logger
     sys.exit(wf.run(main))

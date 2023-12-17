9618f5c125965332eef552aa99cc52747dfff1c4
Klemen Tušar <techouse@gmail.com>
2023-12-17 19:09:48 +0000

    :zap: dispose of AlgoliaSearch object after search completed

--- bin/main_helpers.dart
+++ bin/main_helpers.dart
@@ -29,52 +29,58 @@ void _showPlaceholder() {
 }
 
 Future<void> _performSearch(String query, {String? version}) async {
-  final SearchResponse searchResponse = await AlgoliaSearch.query(
-    query,
-    version: version,
-  );
+  try {
+    final SearchResponse searchResponse = await AlgoliaSearch.query(
+      query,
+      version: version,
+    );
 
-  if (searchResponse.nbHits > 0) {
-    final AlfredItems items = AlfredItems(
-      searchResponse.hits
-          .map((Hit hit) => SearchResult.fromJson(
-              <String, dynamic>{...hit, 'objectID': hit.objectID}))
-          .map(
-            (SearchResult result) => AlfredItem(
-              uid: result.objectID,
-              title: result.prettyTitle,
-              subtitle: result.content.isNotEmpty
-                  ? result.content.truncate(75)
-                  : result.id,
-              arg: result.permalink,
-              text: AlfredItemText(
-                largeType: result.id,
-                copy: result.id,
+    if (searchResponse.nbHits > 0) {
+      final AlfredItems items = AlfredItems(
+        searchResponse.hits
+            .map((Hit hit) =>
+            SearchResult.fromJson(
+                <String, dynamic>{...hit, 'objectID': hit.objectID}))
+            .map(
+              (SearchResult result) =>
+              AlfredItem(
+                uid: result.objectID,
+                title: result.prettyTitle,
+                subtitle: result.content.isNotEmpty
+                    ? result.content.truncate(75)
+                    : result.id,
+                arg: result.permalink,
+                text: AlfredItemText(
+                  largeType: result.id,
+                  copy: result.id,
+                ),
+                quickLookUrl: result.permalink,
+                icon: AlfredItemIcon(path: 'icon.png'),
+                valid: true,
               ),
-              quickLookUrl: result.permalink,
-              icon: AlfredItemIcon(path: 'icon.png'),
-              valid: true,
-            ),
-          )
-          .toList(),
-    );
-    _workflow.addItems(items.items);
-  } else {
-    final Uri url =
-        Uri.https('www.google.com', '/search', {'q': 'Django $query'});
+        )
+            .toList(),
+      );
+      _workflow.addItems(items.items);
+    } else {
+      final Uri url =
+      Uri.https('www.google.com', '/search', {'q': 'Django $query'});
 
-    _workflow.addItem(
-      AlfredItem(
-        title: 'No matching answers found',
-        subtitle: 'Shall I try and search Google?',
-        arg: url.toString(),
-        text: AlfredItemText(
-          copy: url.toString(),
+      _workflow.addItem(
+        AlfredItem(
+          title: 'No matching answers found',
+          subtitle: 'Shall I try and search Google?',
+          arg: url.toString(),
+          text: AlfredItemText(
+            copy: url.toString(),
+          ),
+          quickLookUrl: url.toString(),
+          icon: AlfredItemIcon(path: 'google.png'),
+          valid: true,
         ),
-        quickLookUrl: url.toString(),
-        icon: AlfredItemIcon(path: 'google.png'),
-        valid: true,
-      ),
-    );
+      );
+    }
+  } finally {
+    AlgoliaSearch.dispose();
   }
 }
